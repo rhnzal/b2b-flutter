fbbe069ae13d6d6ba1f21fa0079797fa02bff214
Raihan Rizal <raihanrizal21102003@gmail.com>
2022-09-06 17:05:31 +0700

    submit data

--- lib/register.dart
+++ lib/register.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, body_might_complete_normally_nullable, unrelated_type_equality_checks, empty_statements
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:projectb2b/login.dart';
-
+import 'package:http/http.dart' as http;
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -54,7 +57,8 @@ class _RegisterState extends State<Register> {
         style: TextStyle(color: Colors.white),
         decoration: InputDecoration(
           prefixIcon: Icon(
-            Icons.person_outline, color: Colors.white,
+            Icons.person_outline,
+            color: Colors.white,
           ),
           filled: true,
           fillColor: Color.fromARGB(31, 217, 217, 217),
@@ -72,13 +76,13 @@ class _RegisterState extends State<Register> {
           //   child: Icon(Icons.),
           // )
         ),
-        onChanged: (value){
+        onChanged: (value) {
           setState(() {
             regname = value;
           });
         },
         validator: (value) {
-          if(value!.isEmpty){
+          if (value!.isEmpty) {
             return "Please enter your name";
           }
         },
@@ -92,7 +96,8 @@ class _RegisterState extends State<Register> {
         style: TextStyle(color: Colors.white),
         decoration: InputDecoration(
           prefixIcon: Icon(
-            Icons.alternate_email, color: Colors.white,
+            Icons.alternate_email,
+            color: Colors.white,
           ),
           filled: true,
           fillColor: Color.fromARGB(31, 217, 217, 217),
@@ -110,15 +115,15 @@ class _RegisterState extends State<Register> {
           //   child: Icon(Icons.),
           // )
         ),
-        onChanged: (value){
+        onChanged: (value) {
           setState(() {
             regemail = value;
           });
         },
         validator: (value) {
-          if(value!.isEmpty){
+          if (value!.isEmpty) {
             return "Please enter your email";
-          }else if(!value.contains('@')){
+          } else if (!value.contains('@')) {
             return "Please enter a valid email";
           }
         },
@@ -133,39 +138,50 @@ class _RegisterState extends State<Register> {
         obscureText: _regpass,
         style: TextStyle(color: Colors.white),
         decoration: InputDecoration(
-          prefixIcon: Icon(
-            Icons.key, color: Colors.white,
-          ),
-          filled: true,
-          fillColor: Color.fromARGB(31, 217, 217, 217),
-          contentPadding: EdgeInsets.fromLTRB(30, 10, 20, 10),
-          hintText: 'Password',
-          hintStyle: TextStyle(
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w200,
+            prefixIcon: Icon(
+              Icons.key,
               color: Colors.white,
-              fontSize: 12),
-          border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(20)),
-          suffixIcon: IconButton(onPressed: (){
-            setState(() {
-              _regpass = !_regpass;
-            });
-          }, icon: Icon(_regpass? Icons.visibility_outlined : Icons.visibility_off_outlined, color: Colors.white,), splashColor: Colors.transparent, highlightColor: Colors.transparent,)
-          // prefixIcon: Padding(padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-          //   child: Icon(Icons.),
-          // )
-        ),
-        onChanged: (value){
+            ),
+            filled: true,
+            fillColor: Color.fromARGB(31, 217, 217, 217),
+            contentPadding: EdgeInsets.fromLTRB(30, 10, 20, 10),
+            hintText: 'Password',
+            hintStyle: TextStyle(
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+                fontSize: 12),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(20)),
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _regpass = !_regpass;
+                });
+              },
+              icon: Icon(
+                _regpass
+                    ? Icons.visibility_outlined
+                    : Icons.visibility_off_outlined,
+                color: Colors.white,
+              ),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            )
+            // prefixIcon: Padding(padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            //   child: Icon(Icons.),
+            // )
+            ),
+        onChanged: (value) {
           setState(() {
             regpas = value;
           });
         },
-        validator :(value) {
-          if(value!.isEmpty){
+        validator: (value) {
+          if (value!.isEmpty) {
             return "PLease Enter Your Password";
-          }else if(value.length < 8){
+          } else if (value.length < 8) {
             return "Password must be 8 character or more";
           }
         },
@@ -180,96 +196,123 @@ class _RegisterState extends State<Register> {
         obscureText: _conpass,
         style: TextStyle(color: Colors.white),
         decoration: InputDecoration(
-          prefixIcon: Icon(
-            Icons.lock, color: Colors.white,
-          ),
-          filled: true,
-          fillColor: Color.fromARGB(31, 217, 217, 217),
-          contentPadding: EdgeInsets.fromLTRB(30, 10, 20, 10),
-          hintText: 'Confirm Password',
-          hintStyle: TextStyle(
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w200,
+            prefixIcon: Icon(
+              Icons.lock,
               color: Colors.white,
-              fontSize: 12),
-          border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(20)),
-          suffixIcon: IconButton(onPressed: (){
-            setState(() {
-              _conpass = !_conpass;
-            });
-          }, icon: Icon(_conpass? Icons.visibility_outlined : Icons.visibility_off_outlined, color: Colors.white,), splashColor: Colors.transparent, highlightColor: Colors.transparent,)
-          // prefixIcon: Padding(padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-          //   child: Icon(Icons.),
-          // )
-        ),
+            ),
+            filled: true,
+            fillColor: Color.fromARGB(31, 217, 217, 217),
+            contentPadding: EdgeInsets.fromLTRB(30, 10, 20, 10),
+            hintText: 'Confirm Password',
+            hintStyle: TextStyle(
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+                fontSize: 12),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(20)),
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _conpass = !_conpass;
+                });
+              },
+              icon: Icon(
+                _conpass
+                    ? Icons.visibility_outlined
+                    : Icons.visibility_off_outlined,
+                color: Colors.white,
+              ),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            )
+            // prefixIcon: Padding(padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            //   child: Icon(Icons.),
+            // )
+            ),
         validator: (value) {
-          if(value!.isEmpty){
+          if (value!.isEmpty) {
             return "Please confirm your password";
-          }else if(value != _pasreg){
+          } else if (value != _pasreg.text) {
             return "Password not match";
+          } else {
+            return null;
           }
         },
       ),
     );
-    
-    Widget regButton =Container(
-      margin: EdgeInsets.only(top:40),
-      child: Center(
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(primary: Color.fromARGB(255, 217, 217, 217),shape: StadiumBorder()),
-          onPressed: () {
-            if(formKey.currentState!.validate());
-          },
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-            child: Text('Sign Up',
-                style: TextStyle(
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w700,
-                    color: Color.fromARGB(255, 27, 26, 32),
-                    fontSize: 14)),
-          )),
-    ));
+
+    Widget regButton = Container(
+        margin: EdgeInsets.only(top: 40),
+        child: Center(
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 217, 217, 217),
+                  shape: StadiumBorder()),
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  http.post(Uri.parse("http://192.168.18.37:3000/users"),
+                      headers: {
+                        HttpHeaders.contentTypeHeader: 'application/json'
+                      },
+                      body: json.encode({
+                        "fullname": regname,
+                        "email": regemail,
+                        "password": regpas,
+                        "address": 'jalan mengeugeu' //cuma sementara addressnya masih not null
+                      }));
+                }
+              },
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: Text('Sign Up',
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromARGB(255, 27, 26, 32),
+                        fontSize: 14)),
+              )),
+        ));
 
     Widget loginButton = Container(
       margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Already Have an Account ? ', style: TextStyle(
+          Text(
+            'Already Have an Account ? ',
+            style: TextStyle(
                 fontFamily: 'Inter',
                 fontWeight: FontWeight.w500,
                 fontSize: 11,
-                color: Colors.white),),
+                color: Colors.white),
+          ),
           TextButton(
-                style: ButtonStyle( 
-                  overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  minimumSize: MaterialStateProperty.all(Size.zero),
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  padding: MaterialStateProperty.all(EdgeInsets.zero)
-                ),
-              // style: TextButton.styleFrom(
-              //   padding: EdgeInsets.zero,
-              //   minimumSize: Size.zero,
-              //   tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              // ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Login();
-                }));
-              },
-              child: Text(
-                'Sign In',
-                style: TextStyle(
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 11,
-                    color: Colors.white),
-                
-              ),
+            style: ButtonStyle(
+                overlayColor: MaterialStateProperty.all(Colors.transparent),
+                minimumSize: MaterialStateProperty.all(Size.zero),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                padding: MaterialStateProperty.all(EdgeInsets.zero)),
+            // style: TextButton.styleFrom(
+            //   padding: EdgeInsets.zero,
+            //   minimumSize: Size.zero,
+            //   tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            // ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Login();
+              }));
+            },
+            child: Text(
+              'Sign In',
+              style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 11,
+                  color: Colors.white),
             ),
+          ),
         ],
       ),
     );

875a04d73aeae49d7deef6431ccee10ef1734314
Raihan Rizal <raihanrizal21102003@gmail.com>
2022-11-02 16:47:27 +0700

    detail transaction pay button

--- lib/screen/detailtransaction.dart
+++ lib/screen/detailtransaction.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:intl/intl.dart';
 import 'package:projectb2b/endpoints.dart';
 import 'package:projectb2b/http.dart' as http_test;
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DetailTransaction extends StatefulWidget {
   const DetailTransaction({Key? key, required this.index}) : super(key: key);
@@ -19,7 +20,7 @@ class _DetailTransactionState extends State<DetailTransaction> {
   List data = [];
   String orderId = '';
   String product = '';
-  String status = '';
+  bool status = false;
   String price = '';
   String date = '';
 
@@ -30,14 +31,14 @@ class _DetailTransactionState extends State<DetailTransaction> {
     print(widget.index);
   }
 
-  getActivity() async{
+  Future<void> getActivity() async{
     var response = await http_test.get(url: urlHistory);
     data = response.data;
     if (response.isSuccess){
       data = response.data;
       orderId = data[widget.index]['orderId'].toString();
       product = data[widget.index]['product']['title'].toString();
-      status = data[widget.index]['isPaid'].toString();
+      status = data[widget.index]['isPaid'];
 
       price = NumberFormat.simpleCurrency(locale:'in', decimalDigits: 0).format(data[widget.index]['product']['price']);
       date = DateFormat.yMMMd().format(DateTime.parse(data[widget.index]["createdAt"]));
@@ -83,6 +84,29 @@ class _DetailTransactionState extends State<DetailTransaction> {
 
       }
     }
+  }
+
+  pay() async{
+    var count = 0;
+    Navigator.push(context, MaterialPageRoute(builder: ((context) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: BackButton(
+                onPressed: (){
+                  Navigator.popUntil(context, ((route) {
+                    return count++ == 2;
+                  }));
+                },
+              ),
+              backgroundColor: const Color.fromARGB(255, 23, 22, 29),
+              automaticallyImplyLeading: false,
+            ),
+            body: const WebView(
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: 'https://checkout-staging.xendit.co/web/6322943cd0a17dc369f34252',
+            ),
+          );
+        })));
   }
 
   @override
@@ -227,7 +251,7 @@ class _DetailTransactionState extends State<DetailTransaction> {
             height: 5,
           ),
           Text(
-            status,
+            status.toString(),
             style: const TextStyle(
               fontFamily: 'Inter',
               fontWeight: FontWeight.w700,
@@ -261,6 +285,72 @@ class _DetailTransactionState extends State<DetailTransaction> {
       ),
     );
 
+    Widget payButton = Expanded(
+      child: Align(
+        alignment: FractionalOffset.bottomCenter,
+        child: Container(
+        // alignment: Alignment.bottomCenter,
+        margin: const EdgeInsets.only(bottom: 20),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(255, 23, 22, 29), shape: const StadiumBorder()),
+            onPressed:() async {
+              showDialog(context: context, builder: ((context) => AlertDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                backgroundColor:const Color.fromARGB(255, 224, 232, 235),
+                title: const Text('Confirmation'),
+                titleTextStyle: const TextStyle(
+                    color: Color.fromARGB(255, 23, 22, 29),
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w600),
+                content: const Text('Are You Sure ?'),
+                contentTextStyle: const TextStyle(
+                    color: Color.fromARGB(255, 23, 22, 29)),
+                actions: [
+                    TextButton(
+                        style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(Colors.transparent),
+                            minimumSize: MaterialStateProperty.all(Size.zero),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(0, 0, 10, 10))),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('No',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 23, 22, 29)))),
+                    TextButton(
+                        style: ButtonStyle(
+                            overlayColor: MaterialStateProperty.all(Colors.transparent),
+                            minimumSize: MaterialStateProperty.all(Size.zero),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(0, 0, 10, 10))),
+                        onPressed: () async {
+                          Navigator.pop(context);
+                          pay();
+                        },
+                        child: const Text('Yes',
+                            style: TextStyle(color: Color.fromARGB(255, 23, 22, 29)
+                            )
+                          )
+                        )
+                  ]
+              )));
+            },
+            child: const SizedBox(
+                width: 100,
+                child: Text(
+                  'Pay',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromARGB(255, 224, 232, 235)),
+                ))),
+          ),
+      )); 
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 23, 22, 29),
@@ -276,10 +366,12 @@ class _DetailTransactionState extends State<DetailTransaction> {
       body: isLoad ? const Center(
         child: CircularProgressIndicator(color: Color.fromARGB(255, 23, 22, 29),),
       )
-      :ListView(
+      :Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           card,
-          detail
+          detail,
+          if(!status)...[payButton]
         ],
       ),
     );
